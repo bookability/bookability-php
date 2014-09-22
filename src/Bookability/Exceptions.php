e90d27bf359490a03da67205c7bb8bec3400b698
Daniel Oxenbury <daniel.oxenbury@hotmail.co.uk>
2014-09-22 16:35:26 +0100

    Validation Error

--- src/Bookability/Exceptions.php
+++ src/Bookability/Exceptions.php
@@ -7,4 +7,4 @@ class Bookability_HttpError extends Bookability_Error {}
 /**
  * The parameters passed to the API call are invalid or not provided when required
  */
-class Mailchimp_ValidationError extends Bookability_Error {}
+class Bookability_ValidationError extends Bookability_Error {}
